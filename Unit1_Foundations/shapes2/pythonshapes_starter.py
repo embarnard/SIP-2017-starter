447bd627e5aaaf71be47ffaa97a51bfec40b0025
Emilie Menard Barnard <emiliebarnard@gmail.com>
2017-06-19 17:07:48 -0700

    Fix: No line and so window is large enough

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -1,14 +1,16 @@
 from turtle import *
 import math
 
-# Name your Turtle.
+# Name your Turtle. Right now its name is t.
 t = Turtle()
 
 # Set Up your screen and starting position.
-setup(500,300)
+setup(600,600)
 x_pos = -250
 y_pos = -150
+t.penup()
 t.setposition(x_pos, y_pos)
+t.pendown()
 
 ### Write your code below:
 
